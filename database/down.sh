9b6060736196bd2de349b0d75787eb71cfa604d1
Clive Calmeyer <ccalmeye@thoughtworks.com>
2018-06-15 14:19:49 +0100

    Moved database scripts out of project

--- database/down.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source vars.sh
-
-docker rm -f ${DBNAME}
--- database/vars.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DBUSER=addy
-DBPASS=addypass
-DBNAME=address_book
-DBHOST=localhost
-DBPORT=5432
